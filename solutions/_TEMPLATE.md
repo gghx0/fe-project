da147dab50a8f826e12048b34ac2b7d6655bf371
Gabriel Goh <gghx@gghx-2.local>
2026-08-12 10:34:19 +0800

    #2, #3, #4, #5, #37

--- solutions/_TEMPLATE.md
+++ solutions/_TEMPLATE.md
@@ -1,11 +1,11 @@
 ---
-status: open/solved
+status: solved
 ---
 
 Solution by name.
-
-## Solution
+***
 
 We claim the only solution is $f(x)\equiv x$, which obviously works. We now show it is the only solution. Let $P(x,y)$ denote the assertion that $$. 
 
 We claim the only solutions are $f(x)\equiv x$ and, which obviously work. We now show they are the only solutions. Let $P(x,y)$ denote the assertion that $$. 
+***
--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig } from 'astro/config';
-import { unified } from '@astrojs/markdown-remark';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
-  site: 'https://gghx0.github.io',
-  base: '/fe-project',
+  site: "https://gghx0.github.io",
+  base: "/fe-project",
 
   markdown: {
     processor: unified({
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [rehypeKatex],
+      remarkPlugins: [
+        remarkMath,
+      ],
+
+      rehypePlugins: [
+        rehypeKatex,
+      ],
     }),
   },
 });
